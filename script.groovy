38401e73b6423122abecca30b0254031d83b69ac
Pavansai2926 <89532097+Pavansai2926@users.noreply.github.com>
2021-09-07 14:07:19 +0530

    Update script.groovy
    reenter

--- script.groovy
+++ script.groovy
@@ -10,7 +10,7 @@ def testApp() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: '40b0b10e-64ab-438d-b09e-8a5218d6fafd', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t pavansai2926/ap-1:jma-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push pavansai2926/ap-1:jma-2.0'
