e62a90b1cd3eb21d6fe54fb54aa57715b50fbf00
Pavansai2926 <89532097+Pavansai2926@users.noreply.github.com>
2021-09-07 13:05:59 +0530

    Update script.groovy
    credentials update

--- script.groovy
+++ script.groovy
@@ -10,7 +10,7 @@ def testApp() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: '40b0b10e-64ab-438d-b09e-8a5218d6fafd', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push nanajanashia/demo-app:jma-2.0'
